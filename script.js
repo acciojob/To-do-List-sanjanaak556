cf802715576c506daef2dccf8aa0d59ea7e1c3a6
sanjanaak556 <117841426+sanjanaak556@users.noreply.github.com>
2023-02-26 12:26:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-//your code here
+const todoInput = document.getElementById('newTodoInput');
+const addButton = document.getElementById('addTodoBtn');
+const todoList = document.getElementById('todoList');
 
+addButton.addEventListener('click', () => {
+  const todoText = todoInput.value.trim(); // Remove leading/trailing whitespace
+  if (todoText !== '') {
+    const li = document.createElement('li');
+    li.textContent = todoText;
+    todoList.appendChild(li);
+    todoInput.value = ''; // Clear input
+  }
+});
